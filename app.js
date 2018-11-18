2d527f9f4c0b9eda515c71d4da3d55e64e3b03b1
minaamfasihi <minaam.fasihi@gmail.com>
2018-11-19 01:05:25 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var restify = require('restify');
+var server = restify.createServer();
+
+var users = {};
+var max_user_id = 0;
+
+server.get("/", function(req, res, next) {
+    res.setHeader('content-type', 'application/json');
+    res.writeHead(200);
+    res.end(JSON.stringify(users));
+    return next();
+});
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
+});
